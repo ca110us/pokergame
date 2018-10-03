22b8ae6c2809df10aae3a1e5d253f0a60d566461
edb <b@bbbbbbbb.org>
2018-10-03 18:42:29 +0800

    v0.1.0

--- static/js/sendCard.js
+++ static/js/sendCard.js
@@ -1,4 +1,4 @@
-var socket = io.connect('http://198.13.48.65:1214');
+var socket = io.connect('http://localhost:1214');
 socket.on('connect', function (data) {
   socket.emit('bindSocket', { player: document.getElementById("player").dataset.player});
 });
@@ -10,4 +10,8 @@ socket.on('receiveMessage', function (message) {
 });
 function sendCards() {
     socket.emit('outCards', { player: document.getElementById("player").dataset.player , cards: document.getElementById('cards').value });
+}
+function xsendCards(cards) {
+    socket.emit('outCards', { player: document.getElementById("player").dataset.player , cards: cards });
+    return 'success';
 }
